952134e4e7cdcd9edfb859521644c807a92e871c
Rocky Dey <coderrocky25@gmail.com>
2024-05-16 23:28:27 +0600

    footer section complete

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,10 +1,39 @@
 import "./Footer.css";
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>This is Footer</h1>
-    </div>
+    <footer className='text-color4'>
+      <div className='grid grid-cols-1 md:grid-cols-2'>
+        <div className='bg-color1 py-24'>
+          <div className='lg:w-1/2 ml-auto text-center space-y-6'>
+            <h3 className='uppercase font-medium text-[32px]'>Contact Us</h3>
+            <div className='text-xl font-medium space-y-2'>
+              <p>123 ABS Street, Uni 21, Bangladesh</p>
+              <p>+88 123456789</p>
+              <p>Mon - Fri: 08:00 - 22:00</p>
+              <p>Sat - Sun: 10:00 - 23:00</p>
+            </div>
+          </div>
+        </div>
+        <div className='bg-color2 py-24'>
+          <div className='lg:w-1/2 text-center'>
+            <h3 className='uppercase font-medium text-[32px] mb-6'>
+              Follow Us
+            </h3>
+            <p className='mb-8 text-xl font-medium'>Join us on social media</p>
+            <div className='text-xl flex justify-center items-center gap-8'>
+              <FaFacebookF />
+              <FaInstagram />
+              <FaTwitter />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='bg-color3 py-4 font-medium text-xl text-center'>
+        <p>Copyright &copy; Bistro Boss. All rights reserved.</p>
+      </div>
+    </footer>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      color1: "#1F2937",
+      color2: "#111827",
+      color3: "#151515",
+      color4: "#FFFFFF",
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {

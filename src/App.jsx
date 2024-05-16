8e6d52345523da39e0fdd3a65d0777dd65fceab4
Rocky Dey <coderrocky25@gmail.com>
2024-05-16 23:08:02 +0600

    create and design page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Shared/Header/Header";
+import Footer from "./components/Shared/Footer/Footer";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
